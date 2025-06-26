6cfe929b1f0118299ce5da33a8076efa1089689e
agoscarbone <agoscarbone2709@gmail.com>
2025-06-26 19:17:09 -0300

    segundo commit

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
 #include "lib.h"
 
-typedef struct 
-{
-    uint32_t sample_rate;
-    uint32_t sample_count;
-    uint64_t sound_name[64];
-}encabezado;
-
 int main()
 {
     FILE *f=fopen("audio_list.raw","rb");
@@ -15,6 +8,21 @@ int main()
         printf("\nError\n");
         return -1;
     }
+    pista *p;
+    int cant_pistas=0;
 
+    p=read_file(f,&cant_pistas);
+    
+    for(int i=0; i<cant_pistas; i++)
+    {
+        printf("\nPista %d: %s",i,p[i].enc.sound_name);
+    }
+
+    
+
+
+
+    free(p);
+    fclose(f);
     return 0;
 }
